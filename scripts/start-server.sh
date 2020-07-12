5694c7fbcdb30671aeef8f2761476ca318e5f780
ich777 <ich777@noreply.example.org>
2020-07-12 20:44:18 +0200

    „scripts/start-server.sh“ hinzufügen

---
+++ scripts/start-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "---Sending Pusover message with title: '${PUSHOVER_TITLE}' and message: '${PUSHOVER_MESSAGE}'---"
+wget https://api.pushover.net/1/messages.json --post-data="token=${PUSHOVER_APP_TOKEN}&user=${PUSHOVER_USER_TOKEN}&priority=${PUSHOVER_PRIORITY}&title=${PUSHOVER_TITLE}&message=${PUSHOVER_MESSAGE}" -qO- > /dev/null 2>&1 ; then
+echo "--- D O N E ---"
