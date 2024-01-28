36faf05e95048a4af629e9632d2ed00b63b22696
ich777 <christophhummer@gmail.com>
2024-01-28 21:45:10 +0100

    scripts/start-server.sh aktualisiert

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Replace HOST_IP with the IP from the Host - only works when container is ran on host interface!
+if grep -q "HOST_IP" <<< "${PUSHOVER_MESSAGE}" ; then
+  HOST_IP=$(hostname -I | awk '{print $1}')
+  PUSHOVER_MESSAGE=$(sed "s/HOST_IP/${HOST_IP}/" <<< "${PUSHOVER_MESSAGE}")
+fi
+
 echo "---Sending Pushover message with title: '${PUSHOVER_TITLE}' and message: '${PUSHOVER_MESSAGE}'---"
 wget https://api.pushover.net/1/messages.json --post-data="token=${PUSHOVER_APP_TOKEN}&user=${PUSHOVER_USER_TOKEN}&priority=${PUSHOVER_PRIORITY}&title=${PUSHOVER_TITLE}&message=${PUSHOVER_MESSAGE}" -qO- > /dev/null 2>&1
 echo "--- D O N E ---"
